89198477caf8b81d3d3db377f1c14efa2ba701f1
Lucca Matheus <74078934+luccamat@users.noreply.github.com>
2023-05-07 12:00:35 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,9 +70,42 @@ tabs.forEach(tab =>{
 })
 
 /*==== Modelo serviços ====*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close')
 
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) =>{
+    modalBtn.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () =>{
+        modalViews.forEach((modalView) =>{
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
 /*==== Portfolio swiper ====*/
 
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    loop:true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 /*==== testimonial ====*/
 
 /*==== Scroll Section active link ====*/
